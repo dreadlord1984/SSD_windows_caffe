4181ed771e0f7f95985ea1671e76624627f2bd2a
TaoYang <xuanyuyt@163.com>
2017-11-06 16:51:13 +0800

    复查工作挑出验证集

---
+++ Pedestrian/CHECK/Test_ssd_without_NMS.bat
@@ -0,0 +1,6 @@
+pause
+..\\..\\Build\\x64\\Release2\\test_ssd.exe -confidence_threshold 0.5 ^
+.\\deployD1_noSqrt.prototxt ^
+..\\add_prior_gamma2_D1_new_P5N4D15E4_noSqrt_iter_200000.caffemodel ^
+..\\Data_0922\IOU_ALL_image_List.txt
+pause
