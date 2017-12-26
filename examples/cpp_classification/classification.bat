fb37267f9cd369e83421e8cdaf4965496223006f
xuanyuyt <xuanyuyt@163.com>
2017-12-26 22:23:49 +0800

    MobileNet

---
+++ examples/cpp_classification/classification.bat
@@ -0,0 +1,8 @@
+pause
+..\..\Build\x64\Release\classification.exe ^
+ D:/caffe-master_/models/MobileNet/deploy.prototxt ^
+ D:/caffe-master_/models/MobileNet/mobilenet.caffemodel ^
+ D:/caffe-master_/data/ilsvrc12/imagenet_mean.binaryproto  ^
+ D:/caffe-master_/data/ilsvrc12/synset_words.txt ^
+ D:/caffe-master_/examples/images/cat.jpg
+pause
